aac1ed0c6284813e0e95c902f595143a37491789
KORZH ILLIA <kiev_drum2006@ukr.net>
2022-07-13 11:35:17 +0300

    Filter selector changed

--- src/redux/filtered-contacts-selector.js
+++ src/redux/filtered-contacts-selector.js
@@ -1,4 +1,7 @@
 export const getFilteredContacts = ({ items, filter }) => {
+  if (!filter) {
+    return items;
+  }
   const preparedFilter = filter.toLowerCase();
   return items.filter(contact =>
     contact.name.toLowerCase().includes(preparedFilter)
